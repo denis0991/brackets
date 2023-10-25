e7a060c71f39cef5d459e3466bb664163220eba8
Denis Nasonov <brain0991@gmail.com>
2023-10-25 23:38:46 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, config) {
+  const stack = [];
+  const bracketsPair = {
+    [')']: '(',
+    [']']: '[',
+    ['}']: '{',
+    ['|']: '|'
+    }
+  const openBrackets = config.map(pair => pair[0]);
+  const closingBrackets = config.map(pair => pair[1]);
+
+  for (let i = 0; i < str.length; i++) {
+    const bracket = str[i];
+
+    if (openBrackets.includes(bracket)) {
+      stack.push(bracket);
+    } else {
+      if (stack.length === 0) {
+        return false
+      }
+      let topElement = stack[stack.length - 1]
+      if (bracketsPair[bracket] === topElement) {
+        stack.pop()
+      } else {
+        return false
+      }
+      
+    } 
+  } 
+
+  return stack.length === 0
+  }
+
+
+
