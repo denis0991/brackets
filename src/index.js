55b3b651ba10c29422c326bc70402157cf3bd186
Denis Nasonov <brain0991@gmail.com>
2023-10-28 15:40:30 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 module.exports = function check(str, config) {
   const stack = [];
+  let countStraight = 0;
   const bracketsPair = {
     [')']: '(',
     [']']: '[',
     ['}']: '{',
-    ['|']: '|'
+    ['2']: '1',
+    ['4']: '3',
+    ['6']: '5',
+    ['7']: '7',
+    ['8']: '8',
+    ['|']: '|',
     }
   const openBrackets = config.map(pair => pair[0]);
   const closingBrackets = config.map(pair => pair[1]);
 
+
   for (let i = 0; i < str.length; i++) {
     const bracket = str[i];
-
+   
     if (openBrackets.includes(bracket)) {
-      stack.push(bracket);
+      if (bracketsPair[bracket] == bracket && bracket == stack[stack.length-1]) {
+        stack.pop()
+      } else {
+        stack.push(bracket);
+        console.log(stack)
+      }
+      
+      
     } else {
+      
       if (stack.length === 0) {
         return false
       }
       let topElement = stack[stack.length - 1]
       if (bracketsPair[bracket] === topElement) {
         stack.pop()
-      } else {
+      } 
+      
+       else {
         return false
       }
       
     } 
+    
   } 
 
   return stack.length === 0
